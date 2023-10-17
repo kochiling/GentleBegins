9d627bd8a7f83acb0734f838ddeb799f33f83ae2
SatishPrakasham <112928636+SatishPrakasham@users.noreply.github.com>
2023-10-17 20:14:45 +0800

    god help me

--- app/src/main/java/com/cscorner/gentlebegins/profile_page.java
+++ app/src/main/java/com/cscorner/gentlebegins/profile_page.java
@@ -3,6 +3,7 @@ package com.cscorner.gentlebegins;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class profile_page extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class profile_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        TextView textView = findViewById(R.id.myTextView);
+
+// Set the text using the string resource reference
+        textView.setText(R.string.profile_page_title);
     }
+    // Get a reference to the TextView by its ID
+
+
 }
