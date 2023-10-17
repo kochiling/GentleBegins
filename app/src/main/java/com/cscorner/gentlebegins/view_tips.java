696efc3da255200359be5f576eaa3c801aff7768
Arsyad22 <arsyad7786@yahoo.com>
2023-10-17 19:53:48 +0800

    commit

---
+++ app/src/main/java/com/cscorner/gentlebegins/view_tips.java
@@ -0,0 +1,13 @@
+package com.cscorner.gentlebegins;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class view_tips extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_tips);
+    }
+}
