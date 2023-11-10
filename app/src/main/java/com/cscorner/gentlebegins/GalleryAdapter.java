e0915d7b3edfb27c94c5b907009d79cb9e0f913c
Arsyad22 <arsyad7786@yahoo.com>
2023-11-10 23:17:33 +0800

    commit

---
+++ app/src/main/java/com/cscorner/gentlebegins/GalleryAdapter.java
@@ -0,0 +1,59 @@
+package com.cscorner.gentlebegins;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class GalleryAdapter extends BaseAdapter {
+
+    private ArrayList<GalleryData> dataList;
+    private Context context;
+    LayoutInflater layoutInflater;
+
+    public GalleryAdapter(Context context, ArrayList<GalleryData> dataList) {
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+    @Override
+    public int getCount() {
+        return dataList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (layoutInflater == null){
+            layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        if (view == null){
+            view = layoutInflater.inflate(R.layout.grid_item, null);
+        }
+
+        ImageView gridImage = view.findViewById(R.id.gridImage);
+        TextView gridCaption = view.findViewById(R.id.gridCaption);
+
+
+        Glide.with(context).load(dataList.get(i).getImageURL()).into(gridImage);
+        gridCaption.setText(dataList.get(i).getCaption());
+
+        return view;
+    }
+}
